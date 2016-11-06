875160aeb1c2b4e770cf9148a055a95f64bfef63
Jerry Howard <jerryhoward@fastmail.fm>
2016-11-06 18:56:09 -0500

    Initial commit, including license

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to veggie-lunch
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/veggie_lunch/core_test.clj
@@ -0,0 +1,7 @@
+(ns veggie-lunch.core-test
+  (:require [clojure.test :refer :all]
+            [veggie-lunch.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

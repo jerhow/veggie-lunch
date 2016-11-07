9996d4b9cc3bd21b22d013f5072f4169398bdb2f
Jerry Howard <jerryhoward@fastmail.fm>
2016-11-06 19:07:09 -0500

    Getting started with the project file. Loaded up the anticipated dependencies, at their latest stable versions.

--- project.clj
+++ project.clj
@@ -1,9 +1,17 @@
 (defproject veggie-lunch "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A Slack command integration to help manage the vegetarian lunch orders for our team"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                [compojure "1.5.1"]
+                [http-kit "2.2.0"]
+                [ring/ring-devel "1.5.0"]
+                [ring/ring-core "1.5.0"]
+                [ring/ring-defaults "0.2.1"]
+                [org.clojure/java.jdbc "0.6.1"]
+                [org.xerial/sqlite-jdbc "3.15.1"]
+                [yesql "0.5.3"]]
   :main ^:skip-aot veggie-lunch.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})

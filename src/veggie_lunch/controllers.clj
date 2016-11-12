e84d505444e95933c6ece16b5dfabcb942368467
Jerry Howard <jerryhoward@fastmail.fm>
2016-11-12 17:55:13 -0500

    Parsing and validating the --command

--- src/veggie_lunch/controllers.clj
+++ src/veggie_lunch/controllers.clj
@@ -1,9 +1,38 @@
-(ns veggie-lunch.controllers)
+(ns veggie-lunch.controllers
+  (:require [ring.util.response :refer [response content-type status header]]
+            [clojure.string :refer [split]]))
+
+(def permitted-commands (set ["" 
+                              "--help" 
+                              "--list" 
+                              "--order" 
+                              "--delete" 
+                              "--menu" 
+                              "--lock"
+                              "--unlock"
+                              "--set-menu-url"
+                              "--user-add"
+                              "--user-remove"]))
 
 ; ========================================
 ; HTTP route handlers
 (defn home [request]
-    (str "Veggie-Lunch version " (:app-version request) 
-      "<br /><br />"
-      "Add query string param 'foo' to test:<br />"
-      "foo=" (:foo (:params request))))
+  (let [text-field (:text (:params request))
+        text-parts (split text-field #" ")
+        command (first text-parts)]
+    (println command)
+    (println (contains? permitted-commands command))
+    
+    (if (contains? permitted-commands command)
+      (response (str "\"" command "\"" " valid command"))
+      (response "Error: Sorry, I don't recognize that command."))
+      ; (header (response text-field) "status" "200 OK")
+      ; (header (response text-field) "status" "500 Error"))
+  )
+)
+
+(defn home-ORIG [request]
+  (str "Veggie-Lunch version " (:app-version request) 
+    "<br /><br />"
+    "Add query string param 'foo' to test:<br />"
+    "foo=" (:foo (:params request))))

033f809ad2cb644f3c759e4445b6dc2173621f5a
Jerry Howard <jerryhoward@fastmail.fm>
2016-11-14 23:08:25 -0500

    Started working out the schema for orders. Two new bootstrap queries.

--- src/veggie_lunch/db/queries.sql
+++ src/veggie_lunch/db/queries.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     slack_user_id TEXT NOT NULL DEFAULT (''),
     slack_user_name TEXT NOT NULL DEFAULT (''),
-    created_dt datetime DEFAULT CURRENT_TIMESTAMP NOT NULL, 
+    created_dt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, 
     active BOOLEAN NOT NULL DEFAULT (0),
     full_name TEXT NOT NULL DEFAULT (''),
     CONSTRAINT slack_user_name_unique UNIQUE (slack_user_name)
@@ -21,6 +21,28 @@ VALUES (
     'rn',
     1,
     'Jerry'
+);
+
+-- name: create-table-orders!
+CREATE TABLE IF NOT EXISTS orders (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    created_dt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    order_date TEXT DEFAULT (strftime('%Y-%m-%d', 'now')) NOT NULL,
+    vendor_name TEXT NOT NULL DEFAULT (''),
+    menu_url TEXT NOT NULL DEFAULT (''),
+    CONSTRAINT slack_user_name_unique UNIQUE (order_date, vendor_name)
+);
+
+-- name: create-table-order-items!
+CREATE TABLE IF NOT EXISTS order_items (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    order_id INTEGER NOT NULL,
+    created_dt DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    description TEXT NOT NULL DEFAULT (''),
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (order_id) REFERENCES orders (id),
+    CONSTRAINT slack_user_name_unique UNIQUE (user_id, order_id)
 );
 
 -- name: test-fetch
